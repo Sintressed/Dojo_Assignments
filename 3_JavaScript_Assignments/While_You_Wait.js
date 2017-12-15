291ba9e5f706e50df3c9ca2c7cd6d9d526f88d54
Heidi <hlreidwins@gmail.com>
2017-12-14 22:18:03 -0800

    While_You_Wait.js

---
+++ 3_JavaScript_Assignments/While_You_Wait.js
@@ -0,0 +1,20 @@
+
+ var i = 60;
+ while(i > 0)
+    {
+        if (i >= 30)
+            {
+                console.log("wow " + i + " days? so far awaaayy" );
+                i = i - 1;
+            }
+        else if(i < 30 && i > 5)
+            {
+                console.log( "yayy! Only " + i + " days untll your birthday!");
+                i = i- 1;
+            }
+        else if(i <= 5)
+            {
+                console.log ("WOOOOOOOOOO ONLY " + i + " DAYS TO GOOO")
+                i = i -1;
+            }
+    }
