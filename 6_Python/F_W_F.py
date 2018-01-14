0b13cd9a883218d19781ca82d665ea9937d715c7
Heidi <hlreidwins@gmail.com>
2018-01-14 15:29:15 -0800

    Fun With Functions

---
+++ 6_Python/F_W_F.py
@@ -0,0 +1,17 @@
+def odd_even():
+    for count in range(0,2001):
+        if count % 2 == 0:
+            print 'number is ',count,'. This is an even number'
+        else:
+            print 'number is ',count,'. This is an odd number'
+
+def multiply():
+    a = input()
+    b = []
+    for num in range(0,len(a)):
+        b.append(5 * a[num]) 
+        continue
+    print b
+        
+#odd_even()
+multiply()
