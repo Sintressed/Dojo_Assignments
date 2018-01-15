be7bc5438f97bc83c7f5d6536968a21dab36f94c
Heidi <hlreidwins@gmail.com>
2018-01-15 07:26:05 -0800

    Dictionary Basics

---
+++ 6_Python/DB.py
@@ -0,0 +1,5 @@
+dic = {'name':'Heidi','age':'18','count':'United States','lang':'python'}
+print 'My name is ',dic['name']
+print 'My age is ',dic['age']
+print 'My country of birth is ',dic['count']
+print 'My favorite language is ',dic['lang']
