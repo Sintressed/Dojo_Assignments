d3988455b0cacd00deb8d9989c1c435bf2dfe2b3
Heidi <hlreidwins@gmail.com>
2018-01-24 13:02:16 -0800

    Whats My Name

--- 7_Flask/flask_fundamentals/Whats_My_Name/server.py
+++ 7_Flask/flask_fundamentals/Whats_My_Name/server.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
-# our index route will handle rendering our form
 @app.route('/')
 def index():
   return render_template("index.html")
-  return redirect('/process')
-# this route will handle our form submission
-# notice how we defined which HTTP methods are allowed by this route
-@app.route('/process',methods =['POST'])
-def show_user_profile():
+@app.route('/process', methods=["POST"])
+def process():
+    # do something with the form data 
     name = request.form['name']
-    return render_template("user.html", name )
+    return render_template("user.html",name = name)
+    return redirect('/')
 app.run(debug=True) # run server
 
