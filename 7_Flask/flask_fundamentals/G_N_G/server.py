8094bf9fd8b8928577840fb4f4244fbcf7e35be3
Heidi <hlreidwins@gmail.com>
2018-01-26 12:28:58 -0800

    Great Number Game!

--- 7_Flask/flask_fundamentals/G_N_G/server.py
+++ 7_Flask/flask_fundamentals/G_N_G/server.py
@@ -2,22 +2,21 @@ from flask import Flask, render_template, request, redirect,session
 app = Flask(__name__)
 app.secret_key = 'SecretKey' # you need to set a secret key for security purposes
 # routing rules and rest of server.py  
+import random
 @app.route('/')
-def index():        
-    return render_template("index.html")
-import random  
-x = random.randrange(0, 101)
+def index():       
+    return render_template("index.html") 
+    session['root'] = random.randrange(0, 101)
 @app.route('/users', methods=['POST'])
 def create_user():
-    session['root'] = x
-    if session['root'] != request.form['guess']:
-        if session['root'] > request.form['guess']:
-            session.pop('voice')            
-            session['voice'] = "Too Low"           
-        elif session['root'] < request.form['guess']:
-            session.pop('voice')                        
-            session['voice'] = "Too  High"  
-    elif session['root'] == request.form['guest']:
-        print 'hello'
+    session['x'] = 'GUESS!!'
+    if session['root'] != int(request.form['guess']):
+        if session['root'] >int(request.form['guess']):
+            session['x'] = 'too low! Try Again!'            
+        elif session['root'] < int(request.form['guess']):
+            session['x'] = 'Too High! Try Again!'                                 
+    elif session['root'] == int(request.form['guess']):
+        session['x'] = 'YOU DID IT!! PUT IN A NUMBER AND CLICK HERE TO PLAY AGAIN!>>'
+        session['root'] = random.randrange(0, 101)
     return redirect('/')
 app.run(debug=True)
