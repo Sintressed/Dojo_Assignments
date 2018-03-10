00666832eb806f86f9bc2ec0bd5807e13e166347
Heidi <hlreidwins@gmail.com>
2018-03-09 21:12:35 -0800

    Practice

--- 8_Django/Django_App/apps/blogs_app/views.py
+++ 8_Django/Django_App/apps/blogs_app/views.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-
 # Create your views here.
 from django.shortcuts import render, HttpResponse, redirect
   # the index function is called when root is visited
@@ -9,16 +7,27 @@ def index(request):
     response = "placeholder to later display all the list of blogs"
     return HttpResponse(response)
 def new(request):
-    response =  "placeholder to display a new form to create a new blog" 
+    response = "placeholder to display a new form to create a new blog" 
     return HttpResponse(response)
 def create(request):
-    return redirect('/')
+    if request.method == "POST":
+        print "*"*50
+        print request.POST
+        print request.POST['name']
+        print request.POST['desc']
+        request.session['name'] = "test"  # more on session below
+        print "*"*50
+        request.session['name'] = request.POST['name']
+        request.session['counter'] = 100
+        return redirect("/")
+    else:
+        return redirect("/")
 def show(request,number):
-    response = 'placeholder to display blog '
+    response = "placeholder to display blog "
     return HttpResponse(response + str(number))
 def edit(request,number):
-    response = 'placeholder to edit blog '
+    response = "placeholder to edit blog "
     return HttpResponse(response + str(number))
 def destroy(request,number):
-    return redirect('/')
+    return redirect("/")
 
