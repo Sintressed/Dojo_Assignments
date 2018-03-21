c763e12c9d1693ec1c16b0b7dbd712df7ba9844b
Heidi <hlreidwins@gmail.com>
2018-03-20 20:13:29 -0700

    Semi Users

--- 8_Django/Django_App/apps/semi_users/models.py
+++ 8_Django/Django_App/apps/semi_users/models.py
@@ -2,12 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+import re
 # Create your models here.
+
+
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if len(postData['first_name']) or len(postData['last_name']) or len(postData['email_address'])== 0:
+            errors["name"] = "EYYYYY! DONT PUT ANY EMPTIES IN HERE BOI"
+        else:
+            errors.clear()
+        if not EMAIL_REGEX.match(postData['email_address']):
+            errors["email"] = "Email address invalid"
+        return errors
 class User(models.Model):
     first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
     email_address = models.CharField(max_length = 255)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-    
+    objects = UserManager()
--- 8_Django/Django_App/apps/semi_users/views.py
+++ 8_Django/Django_App/apps/semi_users/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render,redirect,HttpResponse
 from .models import User
 from django.core.urlresolvers import reverse
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request,"semi_users/index.html", {'users' :  User.objects.order_by("id")})
@@ -14,16 +15,29 @@ def identify(request,userid):
 def edit(request,userid):
     return render(request,"semi_users/edit.html",{'userid': userid, 'id' : User.objects.get(id = userid)})
 def destroy(request,userid):
-    return HttpResponse("Destroy")
+    User.objects.get(id = userid).delete()
+    return redirect(reverse("index"))
 #---Post Routes---
 def create(request):
     if request.method == 'POST':
+        User.objects.create(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email_address = request.POST['email_address'],id = User.objects.count() + 1)
         return redirect(reverse("index"))
     else:
         return redirect(reverse("index"))
 def update(request):
     if request.method == 'POST':
-        return redirect(reverse("index"))
+        errors = User.objects.basic_validator(request.POST)
+        if len(errors):
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect(reverse('edit', kwargs={'userid': request.POST['id']}))
+        else:
+            x = User.objects.get(id = request.POST['id'])
+            x.first_name = request.POST['first_name']
+            x.last_name = request.POST['last_name']
+            x.email_address = request.POST['email_address']
+            x.save()
+            return redirect(reverse("index"))
     else:
         return redirect(reverse("index"))
 
